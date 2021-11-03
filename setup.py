411658b4ebcdfbcc5fc23722ddf86322ece3e7bc
xibertao <xibertao@borogroup.co.uk>
2021-11-03 12:13:58 +0000

    Upgraded version with updated requirements.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nf_ea_com_bnop',
-    version='0.1.0-alpha',
+    version='0.1.0-alpha.1',
     description='NF EA Com BNOP',
     author='BORO Solutions',
     author_email='xibertao@borogroup.co.uk',
